fdecf3d5435fd93c567cedfb14b73487f5eba3a9
Sebastian Teo <52814954+YT-Sebastian@users.noreply.github.com>
2020-02-22 10:41:06 +1100

    Add Repo

---
+++ Biorithm/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Rhino;
+using Rhino.Geometry;
+
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+
+namespace Biorithm
+{
+    struct Particle
+    {
+        Point3d position;
+        Point3d particleBest;
+        Vector3d velocity;
+        List<Point3d> history;
+    }
+
+    struct Swarm
+    {
+        List<Particle> bodies;
+        Point3d globalBest;
+    }
+
+
+}
