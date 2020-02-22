d4396755a80d80c8e88ad61733e2aeef7aef1927
Sebastian Teo <52814954+YT-Sebastian@users.noreply.github.com>
2020-02-22 19:09:56 +1100

    Update PSO.cs

--- Biorithm/PSO.cs
+++ Biorithm/PSO.cs
@@ -17,10 +17,11 @@ namespace Biorithm
 {
     public class Particle
     {
-        Point3d position;
-        Point3d particleBest;
-        Vector3d velocity;
-        List<Point3d> history;
+        public Point3d position;
+        public Point3d particleBest;
+        public Vector3d velocity;
+        public List<Point3d> history;
+        public double particleFitnessValue;
 
         public Particle(Point3d startPosition, Vector3d startVelocity)
         {
@@ -33,32 +34,71 @@ namespace Biorithm
 
     public class Swarm
     {
-        List<Particle> bodies;
-        Point3d globalBest = new Point3d(0,0,0);
-        RandomUniformDistribution rnd;
-        double globalFitnessValue;
+        public List<Particle> bodies;
+        public Point3d globalBest = new Point3d(0,0,0);
+        public RandomUniformDistribution rnd;
+        public double globalFitnessValue;
 
         public Swarm(List<Point3d> agents, Point3d fitness)
         {
             rnd = new RandomUniformDistribution(agents.Count);
             globalFitnessValue = globalBest.DistanceTo(fitness);
 
-            foreach (Point3d i in agents)
+            for (int i = 0; i < agents.Count; i++)
             {
-                bodies.Add(new Particle(i, new Vector3d(rnd.Next(), rnd.Next(), rnd.Next())));
-                double particleFitnessValue = i.DistanceTo(fitness);
+                bodies.Add(new Particle(agents[i], new Vector3d(rnd.Next(), rnd.Next(), rnd.Next())));
+                bodies[i].particleFitnessValue = agents[i].DistanceTo(fitness);
 
-                if (particleFitnessValue < globalFitnessValue)
+                if (bodies[i].particleFitnessValue < globalFitnessValue)
                 {
-                    globalFitnessValue = particleFitnessValue;
-                    globalBest = i;
+                    globalFitnessValue = bodies[i].particleFitnessValue;
+                    globalBest = agents[i];
                 }
             }
         }
 
-        public void ParticleSwarmOptimisation()
+        public void ParticleSwarmOptimisation(double particleLearningFactor, double globalLearningFactor, double influenceFactor, double maxVelocity)
         {
+            foreach (Particle i in bodies)
+            {
+                Vector3d toPbest = new Vector3d(i.particleBest - i.position);
+                Vector3d toGbest = new Vector3d(globalBest - i.position);
 
+                double pbestMultiplier = particleLearningFactor * rnd.Next();
+                double gbestMultiplier = globalLearningFactor * rnd.Next();
+
+                Vector3d moveVector = (toPbest * pbestMultiplier) + (toGbest * gbestMultiplier);
+
+                i.velocity += moveVector;
+
+                if (i.velocity.Length > maxVelocity)
+                {
+                    i.velocity *= maxVelocity / i.velocity.Length;
+                }
+            }
+        }
+
+        public void UpdatePosition(Point3d fitness)
+        {
+            foreach (Particle i in bodies)
+            {
+                i.position += i.velocity;
+                i.history.Add(i.position);
+
+                double particleFitnessValue = i.position.DistanceTo(fitness);
+
+                if (particleFitnessValue < i.particleFitnessValue)
+                {
+                    i.particleFitnessValue = particleFitnessValue;
+                    i.particleBest = i.position;
+                }
+
+                if (particleFitnessValue < globalFitnessValue)
+                {
+                    globalFitnessValue = particleFitnessValue;
+                    globalBest = i.position;
+                }
+            }
         }
     }
 
